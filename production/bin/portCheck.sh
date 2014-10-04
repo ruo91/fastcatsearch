882e5094acfb6b7d64e727dfab78268f52cc17ff
Yongbok Kim <ruo91@yongbok.net>
2014-10-04 11:39:17 +0900

    fix - fastcatsearch-server의 버전에 상관없이 불러올수 있도록 수정

--- production/bin/portCheck.sh
+++ production/bin/portCheck.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 cd `dirname $0`/../
 SERVER_HOME=`pwd`
-java -classpath $SERVER_HOME/lib/fastcatsearch-server-2.14.3.jar org.fastcatsearch.util.PingTest "$@"
+java -classpath $SERVER_HOME/lib/fastcatsearch-server-[0-9]*.jar org.fastcatsearch.util.PingTest "$@"
